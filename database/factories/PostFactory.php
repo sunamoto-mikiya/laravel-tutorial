e439862d7d8a329e056aa136665f2930187b565a
sunamoto <mikig@DESKTOP-HDT32N7.localdomain>
2022-04-14 13:29:15 +0900

    Postテーブル ダミーデータ モデルの作成

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $random_date = $this->faker->dateTimeBetween('-1year', '-1day');
+        return [
+            'title' => $this->faker->realText(rand(20, 50)),
+            'body' => $this->faker->realText(rand(100, 200)),
+            'is_public' => $this->faker->boolean(90),
+            'published_at' => $random_date,
+            'created_at' => $random_date,
+            'updated_at' => $random_date
+        ];
+    }
+}
--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -4,17 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Post;
 
-class DatabaseSeeder extends Seeder
+class PostSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(PostSeeder::class);
+        Post::factory()->count(50)->create();
     }
 }
+
+//Post::factory()でPostモデルのFactoryを使う

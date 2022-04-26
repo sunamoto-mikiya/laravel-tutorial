dcc277e01623ccb53ac0eadf70edf819f61bdac0
sunamoto <mikig@DESKTOP-HDT32N7.localdomain>
2022-04-26 10:08:58 +0900

    看板ページの作成

---
+++ resources/views/frontView/posts/task.blade.php
@@ -0,0 +1,204 @@
+<section class="content pb-3">
+    <div class="container-fluid h-100">
+        <div class="card card-row card-secondary">
+            <div class="card-header">
+                <h3 class="card-title">
+                    Backlog
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="card card-info card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Create Labels</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#3</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox1" disabled=""
+                                control-id="ControlID-3">
+                            <label for="customCheckbox1" class="custom-control-label">Bug</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox2" disabled=""
+                                control-id="ControlID-4">
+                            <label for="customCheckbox2" class="custom-control-label">Feature</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox3" disabled=""
+                                control-id="ControlID-5">
+                            <label for="customCheckbox3" class="custom-control-label">Enhancement</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox4" disabled=""
+                                control-id="ControlID-6">
+                            <label for="customCheckbox4" class="custom-control-label">Documentation</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox5" disabled=""
+                                control-id="ControlID-7">
+                            <label for="customCheckbox5" class="custom-control-label">Examples</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Create Issue template</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#4</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox1_1" disabled=""
+                                control-id="ControlID-8">
+                            <label for="customCheckbox1_1" class="custom-control-label">Bug Report</label>
+                        </div>
+                        <div class="custom-control custom-checkbox">
+                            <input class="custom-control-input" type="checkbox" id="customCheckbox1_2" disabled=""
+                                control-id="ControlID-9">
+                            <label for="customCheckbox1_2" class="custom-control-label">Feature Request</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Create PR template</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#6</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card card-light card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Create Actions</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#7</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo ligula eget dolor. Aenean massa.
+                            Cum sociis natoque penatibus et magnis dis parturient montes,
+                            nascetur ridiculus mus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card card-row card-primary">
+            <div class="card-header">
+                <h3 class="card-title">
+                    To Do
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Create first milestone</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#5</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card card-row card-default">
+            <div class="card-header bg-info">
+                <h3 class="card-title">
+                    In Progress
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="card card-light card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Update Readme</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#2</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+                            Aenean commodo ligula eget dolor. Aenean massa.
+                            Cum sociis natoque penatibus et magnis dis parturient montes,
+                            nascetur ridiculus mus.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card card-row card-success">
+            <div class="card-header">
+                <h3 class="card-title">
+                    Done
+                </h3>
+            </div>
+            <div class="card-body">
+                <div class="card card-primary card-outline">
+                    <div class="card-header">
+                        <h5 class="card-title">Create repo</h5>
+                        <div class="card-tools">
+                            <a href="#" class="btn btn-tool btn-link">#1</a>
+                            <a href="#" class="btn btn-tool">
+                                <i class="fas fa-pen"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<?php
+/**
+ * @var Illuminate\Pagination\LengthAwarePaginator|\App\Models\Post[] $posts
+ */
+$title = '投稿一覧';
+?>
+@extends('frontView.layouts.base'){{-- front\layouts\base.blade.phpをここに表示 --}}
+
+@section('content') {{-- @extendsで継承したファイル内の@yieldの部分に@section~@endsectionの部分を埋め込む --}}
+    <div class="card-header">{{ $title }}</div>
+    <div class="card-body">
+        @if ($posts->count() <= 0)
+            <p>表示する投稿はありません。</p>
+        @else
+            <table class="table">
+                @foreach ($posts as $post)
+                    <tr>
+                        <td>{{ $post->published_format }}</td>
+                        {{-- link_to_route(ルート,リンクにしたい文字列,URLに付加したいパラメータ) --}}
+                        <td>{!! link_to_route('posts.show', $post->title, $post) !!}</td>
+                    </tr>
+                @endforeach
+            </table>
+            <div class="d-flex justify-content-center">
+                {{ $posts->links() }}
+            </div>
+        @endif
+    </div>
+@endsection

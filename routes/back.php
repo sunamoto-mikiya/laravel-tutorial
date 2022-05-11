bc751bfb9efed3e076356282ed5891d167662d23
sunamoto <mikig@DESKTOP-HDT32N7.localdomain>
2022-05-11 22:43:47 +0900

    config back.php追記

--- routes/back.php
+++ routes/back.php
@@ -9,3 +9,8 @@ Route::resource('user', 'UserController');
 // Route::get('posts/person/', 'PostController@person')->name('personPost');
 Route::resource('tasks', 'TaskController');
 Route::put('tasks/status/{task}', 'TaskController@status')->name('status');
+
+Route::prefix('slack')->group(function () {
+    Route::get('/index', [SlackController::class, 'index'])->name('slack.index');
+    Route::post('/send', [SlackController::class, 'send'])->name('slack.send');
+});
